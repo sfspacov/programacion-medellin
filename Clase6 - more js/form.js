41b5109e6ec0ba63ff48acbd0083259502a4282c
sfspacov <stephan_fs@hotmail.com>
2023-01-23 17:18:14 -0500

    Clase6 - More js

---
+++ Clase6 - more js/form.js
@@ -0,0 +1,22 @@
+function salvarUsuario()
+{
+	var nombre = document.getElementById("nombre");
+	var email = document.getElementById('correo');
+	var telefono = document.getElementById('telefono');
+	var website = document.getElementById('website');
+	var asunto = document.getElementById('asunto');
+	var mesaje = document.getElementById('mesaje');
+	
+	if (nombre.value == "")
+	{ 
+		alert('Debes escribir tu nombre');
+	}
+	else if (email.value == "")
+	{
+		alert('Debes escribir tu correo');
+	}
+	else
+	{	
+		alert('Datos salvos con suceso');
+	}
+}
