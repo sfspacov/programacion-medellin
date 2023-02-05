cb8ca6d5de76fd899045e505edc8f1086dc80315
claudiapernett <121819028+claudiapernett@users.noreply.github.com>
2023-02-05 11:59:48 -0500

    tarea el html esa malo

---
+++ datos3.js
@@ -0,0 +1,19 @@
+function tarea()
+{
+	var name = document.getElementById("name");
+	var email = document.getElementById("email") ;
+	var message = document.getElementById("message");
+	
+	if (name.value!= "" && 
+	ordenar_valor !=""&&
+	message != "" &&)
+	{	
+	alert('todos los campo estan lleno');
+	}
+
+	
+	else
+	{
+		alert("datos salvos con exito");
+	}
+}
