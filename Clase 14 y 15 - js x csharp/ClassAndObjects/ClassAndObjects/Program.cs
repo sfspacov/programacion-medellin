eb29d0ca5fcd4e411285a7755967c39e88e187a1
sfspacov <stephan_fs@hotmail.com>
2023-04-10 16:02:18 -0500

    Clase de objetos y clase

---
+++ Clase 14 y 15 - js x csharp/ClassAndObjects/ClassAndObjects/Program.cs
@@ -0,0 +1,42 @@
+﻿var carro1 = new Car
+{
+    Marca = "Toyota",
+    Modelo = "Corolla",
+    Ano = 2023,
+    TransmicionAutomatica = true,
+    Precio = 120000000,
+};
+
+var carro2 = new Car
+{
+    Marca = "Volkswagen",
+    Modelo = "Escarabajo",
+    Ano = 1953,
+    TransmicionAutomatica = false,
+    Precio = 18000000,
+};
+
+class Car
+{
+    public string Marca;
+    public string Modelo;
+    public int Ano;
+    public float Precio;
+    public bool TransmicionAutomatica;
+    public bool EstaPrendido;
+
+    void Prender()
+    {
+        EstaPrendido= true;
+    }
+
+    void Apagar()
+    {
+        EstaPrendido = false;
+    }
+
+    void Aceletar()
+    {
+        Console.WriteLine("Vrum, vrum");
+    }
+}
